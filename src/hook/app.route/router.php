3984ceee75240adf17e6115f005d016455435b1d
ebcms <1540837821@qq.com>
2021-10-21 15:40:06 +0800

    1

--- src/hook/app.route/router.php
+++ src/hook/app.route/router.php
@@ -11,7 +11,7 @@ if (!defined('EBCMS_APPS_ROUTER')) {
     App::getInstance()->execute(function (
         Router $router
     ) {
-        $router->getCollector()->addGroup(Helper::getSite(), function (Collector $collector) {
+        $router->getCollector()->addGroup(Helper::getSite(false), function (Collector $collector) {
             $collector->get('/store/{vendor}/{package}', HttpApp::class);
             $collector->get('/store', Index::class);
         });
